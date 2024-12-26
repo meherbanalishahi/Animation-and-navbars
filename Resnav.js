c1181db571dbed22dcf4bd4c6c0e0a62f3b6efd9
meherbanalishahi <meherbanalishahi786@gmail.com>
2024-12-26 05:35:41 -0800

    navbar responsive

---
+++ Resnav.js
@@ -0,0 +1,7 @@
+let icons= document.querySelector('.icons')
+let navbar=document.querySelector('.navbar')
+icons.addEventListener('click',()=>{
+navbar.classList.toggle('showData')
+.console.log(".navbar");
+
+})
---
+++ nav.js
@@ -0,0 +1,12 @@
+const troggle=document.getElementById('icons')
+
+
+const navbar=document.getElementById('menu')
+troggle.addEventListener('click',()=>{
+  
+    navbar.classList.toggle('showData')
+
+    
+})
+
+
