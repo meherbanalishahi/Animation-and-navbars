e1c0579e1b08030559c8f95c6374aa8883fbb3c6
meherbanalishahi <meherbanalishahi786@gmail.com>
2025-01-06 08:57:12 -0800

    working on javascript

---
+++ hotel.js
@@ -0,0 +1,10 @@
+const icon=document.getElementById('troggle')
+
+
+const navbar=document.getElementById('menu')
+    icon.addEventListener('click',()=>{
+        navbar.classList.toggle('showData')
+        console.log(navbar);
+        
+    })
+
