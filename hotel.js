8325b89cd30be7d3669119f9a6c7c0d16bc5da1b
meherbanalishahi <meherbanalishahi786@gmail.com>
2025-01-15 15:24:39 -0800

    full resposive website

--- hotel.js
+++ hotel.js
@@ -1,9 +1,14 @@
 const icon=document.getElementById('troggle')
+const head= document.getElementById('dream')
+console.log(head);
+
 
 
 const bar=document.getElementById('menu')
     icon.addEventListener('click',()=>{
         bar.classList.toggle('showData')
+        head.classList.toggle('dreem')
+
         console.log(bar);
         
     })
